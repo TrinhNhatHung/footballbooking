58c9996a7369a1fef0a7e97ce099bb83be8dc3f8
Hưng Trịnh <nhathungqs@gmail.com>
2022-03-31 22:10:25 +0700

    fix error block CORS

--- footballbooking-pitch-service/src/main/java/com/footballbooking/api/PitchApi.java
+++ footballbooking-pitch-service/src/main/java/com/footballbooking/api/PitchApi.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import com.footballbooking.service.PitchService;
 import com.footballbooking.util.ResponseUtil;
 
 @RestController
+@CrossOrigin
 public class PitchApi {
 	
 	@Autowired
--- footballbooking-user-service/src/main/java/com/footballbooking/api/UserApi.java
+++ footballbooking-user-service/src/main/java/com/footballbooking/api/UserApi.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.footballbooking.service.UserService;
 import com.footballbooking.util.ResponseUtil;
 
 @RestController
+@CrossOrigin
 public class UserApi {
 	
 	@Autowired
